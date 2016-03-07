afcbd0e5ecc457c8b70c31b1005cfee69530d644
Thomas_ROBIN <thomas.robin94@hotmail.fr>
2016-03-07 17:48:44 +0100

    Ajout Formulaire d'enregistrement des page RSS ainsi que le fichier rss ( changement dans les views et controlleur ainsi qu'un nouveau dossier Fichier_lien_rss )

--- src/TestBundle/Controller/TestController.php
+++ src/TestBundle/Controller/TestController.php
@@ -30,4 +30,47 @@ class TestController extends Controller
         return array('feed' => $feed);
     }
 
+
+    /**
+     * @Route("/Formulaire_enregistrement_RSS")
+     * @Template()
+     */
+    public function Formulaire_enregistrement_RSSAction()
+    {
+        $reader = new Reader;
+        // $resource = $reader->download('https://news.ycombinator.com/rss');
+        $resource = $reader->download('http://unodieuxconnard.com/feed/');
+
+        $parser = $reader->getParser(
+            $resource->getUrl(),
+            $resource->getContent(),
+            $resource->getEncoding()
+        );
+
+        $feed = $parser->execute();
+
+        return array('feed' => $feed);
+    }
+    
+
+    /**
+     * @Route("/enregistrement_RSS")
+     * @Template()
+     */
+    public function enregistrement_RSSAction()
+    {
+        $reader = new Reader;
+        // $resource = $reader->download('https://news.ycombinator.com/rss');
+        $resource = $reader->download('http://unodieuxconnard.com/feed/');
+
+        $parser = $reader->getParser(
+            $resource->getUrl(),
+            $resource->getContent(),
+            $resource->getEncoding()
+        );
+
+        $feed = $parser->execute();
+
+        return array('feed' => $feed);
+    }    
 }
