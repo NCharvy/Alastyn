754f3c012d08e27cd6db552cd3dbd2a80fb544d6
Mardaspan <imbert.kevin94@gmail.com>
2016-03-14 11:58:01 +0100

    feat (AdminBundle) Ajout de la fonctionnalité de vérification du rss + ajout de quelques icônes des pays

--- src/Alastyn/AdminBundle/Controller/AdminController.php
+++ src/Alastyn/AdminBundle/Controller/AdminController.php
@@ -26,94 +26,94 @@ class AdminController extends Controller
      */
     public function indexAction(Request $request)
     {
-    	$reader = new Reader;
-        $file=file_get_contents('saved_rss/listeLiens.json');
-        $datas=json_decode($file);
+   //  	$reader = new Reader;
+   //      $file=file_get_contents('saved_rss/listeLiens.json');
+   //      $datas=json_decode($file);
 
-        if ($request->getMethod() == 'POST')
-        {
-            $name = $request->request->get('name');
-            $rss = $request->request->get('rss_link');
+   //      if ($request->getMethod() == 'POST')
+   //      {
+   //          $name = $request->request->get('name');
+   //          $rss = $request->request->get('rss_link');
             
-            $datas[] = array('id'=>(count($datas)),'site'=>$name,'rss'=>$rss); 
-            $textResponse = json_encode($datas,JSON_PRETTY_PRINT);
-            file_put_contents('saved_rss/listeLiens.json', $textResponse);
+   //          $datas[] = array('id'=>(count($datas)),'site'=>$name,'rss'=>$rss); 
+   //          $textResponse = json_encode($datas,JSON_PRETTY_PRINT);
+   //          file_put_contents('saved_rss/listeLiens.json', $textResponse);
 
-            $file=file_get_contents('saved_rss/listeLiens.json');
-            $datas=json_decode($file);
-        }
+   //          $file=file_get_contents('saved_rss/listeLiens.json');
+   //          $datas=json_decode($file);
+   //      }
 
-        $resources = [];
-        foreach ($datas as $data) {
-            $resources[] = $data->rss;
-        }
-        $feeds = [];
+   //      $resources = [];
+   //      foreach ($datas as $data) {
+   //          $resources[] = $data->rss;
+   //      }
+   //      $feeds = [];
 
-        foreach ($resources as $rss) 
-        {
-            $Verfification_rss = $this->get('gbprod.my_service')->Service_verification_rss($rss);
-            if($Verfification_rss == "FLUX RSS VALIDER")
-            {
-				$resource = $reader->download($rss);
+   //      foreach ($resources as $rss) 
+   //      {
+   //          $Verfification_rss = $this->get('gbprod.my_service')->Service_verification_rss($rss);
+   //          if($Verfification_rss == "FLUX RSS VALIDER")
+   //          {
+			// 	$resource = $reader->download($rss);
 				
-				$parser = $reader->getParser(
-					$resource->getUrl(),
-					$resource->getContent(),
-					$resource->getEncoding()
-				);
+			// 	$parser = $reader->getParser(
+			// 		$resource->getUrl(),
+			// 		$resource->getContent(),
+			// 		$resource->getEncoding()
+			// 	);
 				
-				$feed = $parser->execute();
+			// 	$feed = $parser->execute();
 
-				$result = [];
-				for($i = 0;$i<count($feed->items);$i++) {
-					preg_match('/(\<img).*((\/\>)|(\<\/img))/',$feed->items[$i]->content,$result);
-					if (count($result) > 0) {
-						$feed->items[$i]->preimage=$result[0];
-					}else{
-						$feed->items[$i]->preimage="";
-					}
-				}
-				$feeds[] = $feed;
-				return array('feeds' => $feeds);
-			} 
-            else
-            {
-                return array('feeds' => $Verfification_rss);
-            }          
-        }
+			// 	$result = [];
+			// 	for($i = 0;$i<count($feed->items);$i++) {
+			// 		preg_match('/(\<img).*((\/\>)|(\<\/img))/',$feed->items[$i]->content,$result);
+			// 		if (count($result) > 0) {
+			// 			$feed->items[$i]->preimage=$result[0];
+			// 		}else{
+			// 			$feed->items[$i]->preimage="";
+			// 		}
+			// 	}
+			// 	$feeds[] = $feed;
+			// 	return array('feeds' => $feeds);
+			// } 
+   //          else
+   //          {
+   //              return array('feeds' => $Verfification_rss);
+   //          }          
+   //      }
     }
 
     /**
      * @Route("/add_rss")
      * @Template()
      */
-    public function addRssAction(Request $request)
-    {
+  //   public function addRssAction(Request $request)
+  //   {
 
-        $contentFile=file_get_contents("saved_rss/listeLiens.json");
-        $array=json_decode($contentFile);
+  //       $contentFile=file_get_contents("saved_rss/listeLiens.json");
+  //       $array=json_decode($contentFile);
 
-        if ($request->getMethod() == 'POST') 
-        {
-            $URL_SITE_NAME = $_POST["URL_VALEUR_NAME"];
-            $URL_SITE_RSS = $_POST["URL_VALEUR_RSS"];
-            $Verfification_rss = $this->get('gbprod.my_service')->Service_verification_rss($URL_SITE_RSS);
+  //       if ($request->getMethod() == 'POST') 
+  //       {
+  //           $URL_SITE_NAME = $_POST["URL_VALEUR_NAME"];
+  //           $URL_SITE_RSS = $_POST["URL_VALEUR_RSS"];
+  //           $Verfification_rss = $this->get('gbprod.my_service')->Service_verification_rss($URL_SITE_RSS);
 
-       		if($Verfification_rss == "FLUX RSS VALIDER")
-       		{
+  //      		if($Verfification_rss == "FLUX RSS VALIDER")
+  //      		{
 
-				array_push($array,array("id"=>(count($array)),"site"=>$URL_SITE_NAME,"rss"=>$URL_SITE_RSS)); 
-				$textResponse = json_encode($array,JSON_PRETTY_PRINT);
-				file_put_contents("saved_rss/listeLiens.json", $textResponse);
+		// 		array_push($array,array("id"=>(count($array)),"site"=>$URL_SITE_NAME,"rss"=>$URL_SITE_RSS)); 
+		// 		$textResponse = json_encode($array,JSON_PRETTY_PRINT);
+		// 		file_put_contents("saved_rss/listeLiens.json", $textResponse);
 
-       		}
-		}
-		else
-		{
-			$Verfification_rss = "pas de donnée";
-		}
-        return array('file' => $array, 'Verif_Exist' => $Verfification_rss);    
-    }
+  //      		}
+		// }
+		// else
+		// {
+		// 	$Verfification_rss = "pas de donnée";
+		// }
+  //       return array('file' => $array, 'Verif_Exist' => $Verfification_rss);    
+  //   }
 
     /**
      * @Route("/admin/state/create", name="_create_state")
@@ -368,8 +368,11 @@ class AdminController extends Controller
         $form = $this->get('form.factory')->create(FluxType::class, $flow);
 
         if($form->handleRequest($req)->isValid()){
-            $check_rss = $this->get('gbprod.my_service')->Service_verification_rss($rss);
-            $flow->setStatus('Valide');
+            $check_rss = $this->get('gbprod.my_service')->Service_verification_rss($flow->getUrl());
+            $flow->setStatus($check_rss);
+            if($check_rss != 'Valide') {
+                $flow->setPublication(false);
+            }
             $em->persist($flow);
             $em->flush();
 
