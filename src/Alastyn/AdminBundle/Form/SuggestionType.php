c335e177570979e90112aa8993a768caf6a8852e
Mehdi <Mehdi>
2016-03-16 14:58:03 +0100

    feat (FrontBundle) Menu

---
+++ src/Alastyn/AdminBundle/Form/SuggestionType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Alastyn\AdminBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class SuggestionType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('rss')
+            ->add('site')
+            ->add('domaine_existe')
+            ->add('nomDomaine')
+            ->add('adresse')
+            ->add('codepostal')
+            ->add('ville')
+            ->add('nom')
+            ->add('prenom')
+            ->add('courriel')
+            ->add('region', EntityType::class, array(
+                'class'         =>  'AlastynAdminBundle:Region',
+                'choice_label'  =>  'nom',
+                'multiple'      =>  false,
+                'expanded'      =>  false
+            ))
+            ->add('enregistrer', SubmitType::class, array(
+                'attr' => array('class' => 'btn btn-primary')
+            ))
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Alastyn\AdminBundle\Entity\Suggestion'
+        ));
+    }
+}
